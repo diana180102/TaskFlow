c1c5752d7138accee29e288b52c33200b887cf16
Diana <diana101210@gmail.com>
2024-10-25 22:41:42 -0500

    created page of home with buttons of register and login

---
+++ ui/fonts.ts
@@ -0,0 +1,19 @@
+import { Anton, Montserrat, Archivo_Black } from 'next/font/google';
+ 
+export const anton = Anton(
+    { 
+        weight:['400'],
+        subsets: ['latin'] 
+    });
+
+export const monserrat = Montserrat(
+    { 
+        weight:['400'],
+        subsets: ['latin'] 
+    });
+
+export const archivo_black = Archivo_Black(
+    { 
+        weight:['400'],
+        subsets: ['latin'] 
+    });
